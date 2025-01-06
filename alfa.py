0a686335b24d38020cb034bcc8ac7aa039db5d23
joasmund <142325633+joasmund@users.noreply.github.com>
2025-01-06 18:52:18 +0100

    Add oil spill simulation details and update dependencies.

--- alfa.py
+++ alfa.py
@@ -1,4 +1,21 @@
 import meshio
 import numpy as np
+from math import e
 
-data = meshio.read("bay.msh")
+msh = meshio.read("bay.msh")
+
+points = msh.points
+cells = msh.cells
+
+x = 0
+delta_x = 0.1
+t = 0.01
+
+def oil_distribution():
+    return e ** (-((abs(x-delta_x))**2 / t))
+
+def flow_rate():
+    return np.array[[y-0.2*x][-x]]
+
+
+
