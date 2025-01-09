fdc5b57eb34ac9c53e06fa084a4315e2e3a492df
Sebastian Sverkmo <sebastian.sverkmo@outlook.com>
2025-01-09 13:02:12 +0100

    Added some comments, finding normal and direction

--- cell.py
+++ cell.py
@@ -5,45 +5,49 @@ class Cell:
         self.point = np.array(points)
         self.midpoint = self.midpoint_calc()
         self.triangle = self.triangle_calc()
-        self.norm = self.unit_normal_vector()
-
+        self.norm = self.unit_normal_vector(self.point[0], self.point[1])
+    
+    # Calculate the triangle
     def triangle_calc(self):
         x1, y1 = self.point[0]
         x2, y2 = self.point[1]
         x3, y3 = self.point[2]
         return 0.5 * abs((x1 - x3) * (y2 - y1) - (x1 - x2) * (y3 - y1))
-
+    
+    # Calculate the midpoint 
     def midpoint_calc(self):
         return np.mean(self.point, axis=0)
     
-    def unit_normal_vector(point1, point2):
+    # Finding the normal
+    def unit_normal_vector(self, point1, point2):
         vector = point2 - point1
         normal_vector = np.array([-vector[1], vector[0]])
         return normal_vector / np.linalg.norm(normal_vector)
     
-    def check_vector_direction(m, pov, es, ee): # midpoint, point on vector, edge start and edge end.
+    # Checking the direction 
+    def check_vector_direction(self, m, pov, es, ee): # midpoint, point on vector, edge start and edge end.
         vector = np.array(pov) - np.array(m)
-        normal = unit_normal_vector(es, ee)
+        normal = Cell.unit_normal_vector(es, ee)
         edge_length = np.linalg.norm(np.array(ee)-np.array(es))
         
         scaled_normal = normal * edge_length
-
+        
+        # Dot  production for direction 
         dot_product = np.dot(vector, scaled_normal)
         if dot_product > 0:
-            direction = "Same as normal vector"
+            direction = m * dot_product, "Same as normal vector"
         elif dot_product < 0:
-            direction = "Opposit of normal vector"
+            direction = pov * dot_product, "Opposit of normal vector"
         else:
             direction = "Orthogona"
         return dot_product, direction
     
-
+# Using fixed coordinates try the function
 points = [(0, 0), (1, 0), (0, 1)]
 cell = Cell(points)
 
 
 print("Midtpunkt:", cell.midpoint)  
 print("Areal:", cell.triangle)   
-print("Normale vektorer og skalerte vektorer:")
-for i, normal in enumerate(cell.norm[0], start=1): 
-    print(f"n{i}: {normal}, scaled: {cell.norm[1][i-1]}")
+print("Normale vektorer", cell.norm)
+
