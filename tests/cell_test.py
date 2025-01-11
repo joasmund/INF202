80d1f2f855f5b68fe734c5c4ca6c42b37f076fd9
joasmund <142325633+joasmund@users.noreply.github.com>
2025-01-11 13:39:19 +0100

    fix: temporarily add scripts directory to system path for imports in tests

--- tests/cell_test.py
+++ tests/cell_test.py
@@ -1,5 +1,14 @@
-from ..structure_start import Cell, Line, Triangle
 import pytest
+import sys
+import os
+
+# Add the scripts directory to the system path #This is temporary please remove when importing is fixed
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'scripts')))
+
+from structure_start import Cell, Line, Triangle
+
+
+
 
 def test_positive_length(input_n, expected):
     print(f"{input_n}")
@@ -15,4 +24,4 @@ def test_line():
     l = Line([1, 2], 1)
     assert l.__str__() == "Line with id 1: []"
 
-test_line()
+def test_midpoint():
