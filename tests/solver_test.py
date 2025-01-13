7a71f41547ad7cf53f128ff7dba3b5f5320b0f6e
joasmund <142325633+joasmund@users.noreply.github.com>
2025-01-13 12:43:38 +0100

    test: added test for
    test: started on tests for the solver file, and implemented a test for
    cell factory register function

---
+++ tests/solver_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+
+from src.Simulation.solver import CellFactory 
+from src.Simulation.cells import Cell, Vertex, Line, Triangle
+
+@pytest.fixture
+def cell_factory():
+    factory = CellFactory()
+    factory.register("Vertex", Vertex)
+    factory.register("Line", Line)
+    factory.register("Triangle", Triangle)
+    return factory
+
+@pytest.mark.parametrize(
+    "cell_type, pts, idx", [("Vertex", [1], 1), 
+                            ("Line", [1, 2], 1), 
+                            ("Triangle", [1, 2, 3], 1)])
+def test_register(cell_factory, cell_type, pts, idx):
+    assert cell_factory._cellTypes == {"Vertex": Vertex, "Line": Line, "Triangle": Triangle}
+
+
