c59852aa7702d89f7628d6c6e16eaab0bbf77f18
ioeftedal <ivar.odegardstuen.eftedal@nmbu.no>
2025-01-14 14:45:10 +0100

    feat: rewrote the find neighbor function to a mucher faster alternative
    Rewrote the find neighbor funtion to take use of the nodes directly
    retrieved from the dict stored using the meshio biult in function.
    
    Created faces from the nodes and found neighbors based on the faces
    (edges). Went from 8.5 seconds to 0.028 seconds!

--- src/Simulation/mesh.py
+++ src/Simulation/mesh.py
@@ -1,53 +1,10 @@
 import meshio
 import numpy as np
 
-from .cells import Line, Triangle, Vertex
-from .solver import CellFactory
-
 
 class Mesh:
-    def __init__(self, mshName) -> None:
-        """
-        Constructor of mesh class reads in and creates the
-        computational mesh and stores points and cells
-        :param mshName: name of the .msh file that needs to be stored
-        """
-        self._msh = meshio.read(mshName)  # Store msh as an instance variable
+    def __init__(self, meshName) -> None:
+        self._mesh = meshio.read(meshName)
 
-        cells = self._msh.cells
-
-        self._points = np.array(
-            np.vstack(self._msh.points)[:, :2]
-        )  # List of all points
-
-        cf = CellFactory()
-
-        cf.register("vertex", Vertex)
-        cf.register("line", Line)
-        cf.register("triangle", Triangle)
-
-        self._cells = []  # List of all cells
-
-        for cellForType in cells:
-            cellType = cellForType.type  # Type of cell (Vertex, Line, Triangle)
-            for pts in cellForType.data:
-                idx = len(self._cells)
-                self._cells.append(cf(cellType, pts, idx, self.coordinates))
-
-    def computeNeighbors(self):
-        """
-        Calls computeNeighbors function for every cell
-        """
-        [cell.computeNeighbors(self._cells) for cell in self._cells]
-
-    @property
-    def coordinates(self):
-        """
-        Turn the arrays into a single numpy array
-        and remove the z-axis
-        """
-        return self._points
-
-    @property
-    def cells(self):
-        return self._cells
+        self._points = np.array(np.vstack(self._mesh.points[:, :2]))
+        cells = self._mesh.cells
