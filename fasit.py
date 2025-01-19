7a455c7605afd8969272aca933a76ac3d65037fb
joasmund <142325633+joasmund@users.noreply.github.com>
2025-01-19 21:52:45 +0100

    fix: fixed arpgarse conflicts
    I think all comflicts have been resolved regarding the different command
    line arguments. input.toml is set as default, per the task description

--- fasit.py
+++ fasit.py
@@ -4,9 +4,9 @@ import argparse
 
 def parseInput():
     parser = argparse.ArgumentParser(description='The code will use default values if no arguments are given.')
-    parser.add_argument('--find_all', default='src', help='Search for and run all config files in the main program folder with --find_all')
-    parser.add_argument('-f', '--folder', default='src', help='Put in name of the folder where the config files are located.')
     parser.add_argument('-c', '--config_file', default='input.toml', help='Put in name of the config file.')
+    parser.add_argument('-f', '--folder', type=str, help='Put in name of the folder where the config files are located.')
+    parser.add_argument('--find_all', action="store_true", help='Search for and run all config files in the main program folder')
     args = parser.parse_args()
     return args
 
@@ -33,7 +33,7 @@ def file_search(directory):
     return files
 
 def get_search_directory(args):
-    if args.folder != 'src':  # Check if a custom folder was provided
+    if args.folder:  # Check if a custom folder was provided
         return args.folder
     return "./"  # Default to root directory
 
@@ -49,14 +49,24 @@ def create_subfolder_from_file(file_path):
 def main():
     args = parseInput()
     search_dir = get_search_directory(args)
-    files = file_search(search_dir)
-    toml_files = [file for file in files if file.endswith(".toml")]
-    print(toml_files)
+    if args.find_all or args.folder:
+        toml_files = file_search(search_dir)
+        toml_files = [file for file in toml_files if file.endswith(".toml")]
+        print(toml_files)
 
-    for file in toml_files:
-        data = toml.load(file)
-        print(data)
-        create_subfolder_from_file(file)
+        for file in toml_files:
+            file_path = os.path.join(search_dir, file)
+            data = toml.load(file_path)
+            print(data)
+            create_subfolder_from_file(file_path)
+    else:
+        config_file = os.path.join(search_dir, args.config_file)
+        if os.path.exists(config_file):
+            data = toml.load(config_file)
+            print(data)
+            create_subfolder_from_file(config_file)
+        else:
+            print(f"Config file {config_file} does not exist.")
 
 if __name__ == "__main__":
     main()
